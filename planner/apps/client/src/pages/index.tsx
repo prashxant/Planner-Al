71db42bdf1ae5e1106fb0f9398220f5e6f2fe3e6
prashxant <notxdead0@gmail.com>
2025-03-06 18:16:54 +0530

    imported intenal package / working Properly

--- planner/apps/client/src/pages/index.tsx
+++ planner/apps/client/src/pages/index.tsx
@@ -1,12 +1,14 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "@/styles/Home.module.css";
+import { Button } from "@repo/ui/button";
 
 
 
 export default function Home() {
   return (
     <>
+<Button appName="client">Click me</Button>
       hello world
     </>
   );
