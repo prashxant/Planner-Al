25e27681e0f6ea2dbaa41fd2acdad84eeacc1d75
prashxant <notxdead0@gmail.com>
2025-03-08 07:15:06 +0530

    checkpoint-adding mongodb

--- planner/apps/client/src/pages/SignupPage.tsx
+++ planner/apps/client/src/pages/SignupPage.tsx
@@ -1,13 +1,20 @@
 import { Password } from "@mui/icons-material";
 import { Signup } from "@repo/ui/Signup";
+import axios from 'axios';
 
 export default function SignupPage() {
     return(
         <>
-            <Signup onClick ={(username, Password)=>{
+            <Signup onClick ={ async (username, Password)=>{
+
+                // const response = await axios.post("admin.kirat.com/signup",{
+                //     username,
+                //     Password
+                // }) 
+            }
 
 
-            }}
+            }
             />
         </>
     )
